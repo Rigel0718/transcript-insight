b0677bb90dfb0734945b711c2de56041af15e0dd
geonhee <pollux0718@gmail.com>
2025-05-29 21:19:27 +0900

    Add Element class to parser/element.py

---
+++ parser/element.py
@@ -0,0 +1,17 @@
+from typing import List, Dict, Optional
+from pydantic import BaseModel
+
+
+class Element(BaseModel):
+    category: str  # table, figure, chart, heading1, header, footer, caption, paragraph, equation, list, index, footnote
+    content: str = ""
+    html: str = ""
+    markdown: str = ""
+    base64_encoding: Optional[str] = None
+    image_filename: Optional[str] = None
+    id: Optional[int] = None
+    coordinates: Optional[List[Dict]] = None
+    entity: str = ""
+
+    def copy(self) -> "Element":
+        return self.model_copy(deep=True)
