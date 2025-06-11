fd1462b68a78b0626ba744371a9b4af001b6b811
geonhee <pollux0718@gmail.com>
2025-06-11 20:43:25 +0900

    Add ParserConfig enum class for managing input and output configurations with START and END values

--- parser/manage_enum.py
+++ parser/manage_enum.py
@@ -1,8 +1,15 @@
 from langgraph.graph import END as LANG_END  # 이름 구분
+from langgraph.graph import START as LANG_START
 from enum import Enum
 
 
 class OcrRoute(str, Enum):
     "OCR을 해야하는지 판단하는 edge에 가는 route의 tool name"
     TOOLS = "OCRtools"
+    END = LANG_END
+
+
+class ParserConfig(Enum):
+    "sub graph단위의 input, output config 관리"
+    START = LANG_START
     END = LANG_END
