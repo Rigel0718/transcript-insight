9460887f2bb603f6030b6cd40ffd8551fa1b3a7f
geonhee <pollux0718@gmail.com>
2025-06-11 19:14:44 +0900

    Rename Route enum class to OcrRoute and update TOOLS value for OCR tool identification

--- parser/manage_enum.py
+++ parser/manage_enum.py
@@ -2,6 +2,7 @@ from langgraph.graph import END as LANG_END  # 이름 구분
 from enum import Enum
 
 
-class Route(str, Enum):
-    TOOLS = "tools"
+class OcrRoute(str, Enum):
+    "OCR을 해야하는지 판단하는 edge에 가는 route의 tool name"
+    TOOLS = "OCRtools"
     END = LANG_END
