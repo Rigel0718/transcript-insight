2218b6049ec04dc5ce35ebf0083c6cbd8323d89b
geonhee <pollux0718@gmail.com>
2025-06-14 17:26:27 +0900

    Add GroupXYLine initialization in graph.py for enhanced OCR data processing

--- parser/graph.py
+++ parser/graph.py
@@ -1,7 +1,9 @@
 from .upstage_parser import UpstageOCRNode
-from .ocrjsonparser  import group_by_xy_lines
+from .ocrjsonparser  import GroupXYLine
 import os
 
 upstage_ocr_node = UpstageOCRNode(
     api_key=os.environ["UPSTAGE_API_KEY"], verbose=True
 )
+
+group_xy_line_node = GroupXYLine(verbose=True)
