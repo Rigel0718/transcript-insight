e209a58ed98539e64ba66c62025c49501895afdb
geonhee <pollux0718@gmail.com>
2025-06-14 17:21:46 +0900

    Add UpstageOCRNode initialization with API key and verbose mode in graph.py

--- parser/graph.py
+++ parser/graph.py
@@ -1,3 +1,7 @@
 from .upstage_parser import UpstageOCRNode
+from .ocrjsonparser  import group_by_xy_lines
+import os
 
-
+upstage_ocr_node = UpstageOCRNode(
+    api_key=os.environ["UPSTAGE_API_KEY"], verbose=True
+)
