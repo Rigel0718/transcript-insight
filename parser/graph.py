308e90bff065a7f74b5bdd3059a0c7a6eca9c97f
geonhee <pollux0718@gmail.com>
2025-06-16 00:02:00 +0900

    Add edges and set entry point in ocr_json_workflow for improved processing flow

--- parser/graph.py
+++ parser/graph.py
@@ -17,3 +17,8 @@ ocr_json_workflow = StateGraph(OCRJsonState)
 ocr_json_workflow.add_node('upstage_ocr_parser', upstage_ocr_node)
 ocr_json_workflow.add_node('group_by_xy', group_xy_line_node)
 ocr_json_workflow.add_node('ocr_extract_llm', ocr_extract_json_node)
+
+ocr_json_workflow.add_edge('upstage_ocr_parser', 'group_by_xy')
+ocr_json_workflow.add_edge('group_by_xy', 'ocr_extract_llm')
+
+ocr_json_workflow.set_entry_point('upstage_ocr_parser')
