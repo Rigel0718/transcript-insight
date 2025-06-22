17677b034903efb641cb0e7857ae643fbe5c4b63
geonhee <pollux0718@gmail.com>
2025-06-23 03:21:25 +0900

    Update category comment in Element class for clarity

--- parser/element.py
+++ parser/element.py
@@ -4,15 +4,12 @@ from pydantic import BaseModel
 
 
 class Element(BaseModel):
-    category: str  # table, figure, chart, heading1, header, footer, caption, paragraph, equation, list, index, footnote
+    category: str  # upstage-doc-parser categories : table, figure, chart, heading1, header, footer, caption, paragraph, equation, list, index, footnote
     content: str = ""
-    html: str = ""
     markdown: str = ""
     base64_encoding: Optional[str] = None
-    image_filename: Optional[str] = None
     id: Optional[int] = None
     coordinates: Optional[List[Dict]] = None
-    entity: str = ""
 
     def copy(self) -> Element:
         return self.model_copy(deep=True)
