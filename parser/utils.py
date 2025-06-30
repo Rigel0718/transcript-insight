13683bd678bd23f6ca70944fd85c2c4871a8b57c
geonhee <pollux0718@gmail.com>
2025-06-30 19:48:30 +0900

    Add utility functions for loading chat prompts from YAML files

---
+++ parser/utils.py
@@ -0,0 +1,16 @@
+import yaml
+from langchain_core.prompts import ChatPromptTemplate
+
+# base_dir = os.path.dirname(__file__)  # 이 파일 기준
+
+def get_chat_prompt_yaml(file_path):
+    with open(file_path, "r", encoding='utf8') as f:
+        yaml_content = yaml.safe_load(f)
+        return [(message['role'], message['content']) for message in yaml_content['messages']]
+
+
+def load_prompt_template(prompt_filename: str) -> ChatPromptTemplate:
+    # prompt_path = os.path.join(base_dir, "prompts", prompt_filename)
+    prompt_yaml = get_chat_prompt_yaml(prompt_filename)
+    print(prompt_yaml)
+    return ChatPromptTemplate.from_messages(prompt_yaml)
