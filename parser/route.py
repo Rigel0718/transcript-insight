a71ee7461068e0d0917c55edf92f852d02d906e8
geonhee <pollux0718@gmail.com>
2025-07-02 03:10:22 +0900

    Refactor need_ocr_tool function to return boolean value indicating OCR necessity

--- parser/route.py
+++ parser/route.py
@@ -1,12 +1,6 @@
 from .state import ParseState
-
-def need_ocr_tool(state: ParseState):
-    if state["unstructured_elements"] == "<<Emty>>":
-        return False
-    else:
-        return True
     
-def route_table(state: ParseState) -> str:
+def need_ocr_tool(state: ParseState) -> str:
     if len(state.get("needs_ocr_elements", [])) > 0:
-        return "ocr_parsing_node"
-    return "next_node"
+        return True
+    return False
