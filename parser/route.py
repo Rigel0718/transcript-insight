3b19b09cdaf3d3653196ccf9c2888907fa33e2aa
geonhee <pollux0718@gmail.com>
2025-07-02 03:05:10 +0900

    Add route_table function to determine next processing node based on OCR needs

--- parser/route.py
+++ parser/route.py
@@ -5,3 +5,8 @@ def need_ocr_tool(state: ParseState):
         return False
     else:
         return True
+    
+def route_table(state: ParseState) -> str:
+    if len(state.get("needs_ocr_elements", [])) > 0:
+        return "ocr_parsing_node"
+    return "next_node"
