814d17931eee05f8aa9fafe0e34b70222946a322
geonhee <pollux0718@gmail.com>
2025-07-07 18:45:18 +0900

    Add FastAPI initialization and include API router in main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from fastapi import FastAPI
+from api.route import router
+
 def main():
     print("Hello from transcript-insight!")
-
+    app = FastAPI()
+    app.include_router(router)
 
 if __name__ == "__main__":
     main()
