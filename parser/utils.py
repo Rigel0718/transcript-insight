7d3a7176d8f2c4ee455e9b869fc3abe5299988a3
geonhee <pollux0718@gmail.com>
2025-07-12 18:04:04 +0900

    Refactor prompt template loading to use the correct file path and improve code clarity.

--- parser/utils.py
+++ parser/utils.py
@@ -1,7 +1,8 @@
 import yaml
 from langchain_core.prompts import ChatPromptTemplate
+import os
 
-# base_dir = os.path.dirname(__file__)  # 이 파일 기준
+base_dir = os.path.dirname(__file__)
 
 def get_chat_prompt_yaml(file_path):
     with open(file_path, "r", encoding='utf8') as f:
@@ -10,6 +11,6 @@ def get_chat_prompt_yaml(file_path):
 
 
 def load_prompt_template(prompt_filename: str) -> ChatPromptTemplate:
-    # prompt_path = os.path.join(base_dir, "prompts", prompt_filename)
-    prompt_yaml = get_chat_prompt_yaml(prompt_filename)
+    prompt_path = os.path.join(base_dir, prompt_filename)
+    prompt_yaml = get_chat_prompt_yaml(prompt_path)
     return ChatPromptTemplate.from_messages(prompt_yaml)
