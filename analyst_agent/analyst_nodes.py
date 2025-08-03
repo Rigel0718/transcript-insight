0e49033e411d0377b1f0d1cdb102a12d19bb5020
geonhee <pollux0718@gmail.com>
2025-08-03 17:49:33 +0900

    Add llm initialization in QueryReWrite and DataFrameExtractorNode classes for enhanced query processing using ChatOpenAI.

--- analyst_agent/analyst_nodes.py
+++ analyst_agent/analyst_nodes.py
@@ -1,14 +1,24 @@
 from .base import BaseNode
 from .state import Text2ChartState
-
+from langchain_openai import ChatOpenAI
+from langchain_core.language_models.chat_models import BaseChatModel
+from typing import Optional
 
 class QueryReWrite(BaseNode):
     '''
     유저의 쿼리를 LLm을 활용하여 Agent가 이해할 수 있게 재 구성해주는 Node.
     '''
 
-    def __init__(self, verbose=False, **kwargs):
+    def __init__(self, llm: Optional[BaseChatModel] = None, verbose=False, **kwargs):
         super().__init__(verbose=verbose, **kwargs)
+        self.llm = llm or self._init_llm()
+
+    def _init_llm(self):
+        llm = ChatOpenAI(
+            model="gpt-4o",
+            temperature=0,
+        )
+        return llm 
 
     def run(self, state: Text2ChartState):
         ...
@@ -19,9 +29,16 @@ class DataFrameExtractorNode(BaseNode):
     LLM을 활용하여 dataframe으로 추출해주는 코드를 생성해주는 Node.
     '''
 
-    def __init__(self, verbose=False, **kwargs):
+    def __init__(self, llm: Optional[BaseChatModel] = None, verbose=False, **kwargs):
         super().__init__(verbose=verbose, **kwargs)
+        self.llm = llm or self._init_llm()
 
+    def _init_llm(self):
+        llm = ChatOpenAI(
+            model="gpt-4o",
+            temperature=0,
+        )
+        return llm 
 
     def run(self, state: Text2ChartState):
         ...
