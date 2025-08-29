81670415a247f6f60755c6887c45239a8b42ba49
Shin GeinHee <pollux0718@gmail.com>
2025-08-29 18:49:52 +0900

    refactor(state): remove chart_path and csv_path fields from ReportState

--- analyst_agent/state.py
+++ analyst_agent/state.py
@@ -1,6 +1,5 @@
-from typing import TypedDict, Annotated, List, Dict
-import operator
-from analyst_agent.report_plan_models import ReportPlan, ChartSpec, TableSpec, AnalysisSpec, MetricSpec, InformMetric
+from typing import TypedDict, Annotated, List
+from analyst_agent.report_plan_models import AnalysisSpec, MetricSpec, InformMetric, ReportPlan
 
 class ReportState(TypedDict, total=False):
     # user_input
@@ -9,11 +8,9 @@ class ReportState(TypedDict, total=False):
     run_id: Annotated[str, "Unique run identifier"] = ''
 
     # report_element
-    report_plan: Annotated[ReportPlan, "Report plan (as a dictionary or serialized string)"] = ''
+    report_plan: Annotated[ReportPlan, "List of MetricInsight"] = []
     metric_plan: Annotated[List[MetricSpec], "List of MetricSpec(id, rationale, compute_hint, chart_type, produces, tags)"] = []
     inform_metric: Annotated[InformMetric, "Normalized student profile & transcript summary (name, university, department, dates, credits, GPA, percentage)"] = ''
-    chart: Annotated[ChartSpec, "generated chart spec(name, chart_type, df_ref, options)"] = ''
-    table: Annotated[TableSpec, "generated table spec(name, desc, query, constraints)"] = ''
     analyst: Annotated[AnalysisSpec, "Config for Analysis spec (focus, audience, tone, language)"] = ''
 
     rewrite_query: Annotated[str, "Rewritten query"] = ''
