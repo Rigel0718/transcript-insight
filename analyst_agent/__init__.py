cd9c64eae5ced3e28f91f9ce4516e679467d3e7a
Shin GeinHee <pollux0718@gmail.com>
2025-09-02 21:31:17 +0900

    refactor(agent): export AnalysisSpec in transcript_analyst_agent package

--- analyst_agent/__init__.py
+++ analyst_agent/__init__.py
@@ -1,7 +1,9 @@
 from .graph import transcript_analyst_graph
 from .state import ReportState
+from .report_plan_models import AnalysisSpec
 
 __all__ = [
     'transcript_analyst_graph',
-    'ReportState'
+    'ReportState',
+    'AnalysisSpec',
 ]
