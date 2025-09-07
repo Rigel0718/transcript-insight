b127651a4d69d1eb6fd4da2626bc924db12dd033
Shin GeinHee <pollux0718@gmail.com>
2025-09-08 03:46:37 +0900

    feat(api): serve chart images via StaticFiles in main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
-from api.route import router
+from api.route import router, CLIENT_DATA_DIR
 from dotenv import load_dotenv
+from fastapi.staticfiles import StaticFiles
 
 load_dotenv()
 
 app = FastAPI()
 app.include_router(router)
+app.mount(
+    "/artifacts",
+    StaticFiles(directory=str(CLIENT_DATA_DIR / "users")),
+    name="artifacts"
+)
