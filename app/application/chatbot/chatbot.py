cc9348d0c31a2b6a9a8d37b13eb1ff5e85532f39
geonhee <pollux0718@gmail.com>
2025-10-21 04:10:32 +0900

    feat(graph): create chatbot graph with ChatbotNode

--- app/application/chatbot/chatbot.py
+++ app/application/chatbot/chatbot.py
@@ -1,6 +1,19 @@
-from typing import Annotated, TypedDict
-from langgraph.graph import StateGraph, START, END, MessagesState
-from langchain_core.messages import HumanMessage, SystemMessage
-from langgraph.graph.message import add_messages
+from app.application.chatbot.chatbot_node import ChatbotNode
+from langgraph.checkpoint.memory import MemorySaver
+from app.application.chatbot.state import ChatbotState
+from langgraph.graph.state import CompiledStateGraph
+from queue import Queue
+from app.core.env_model import Env
+from langgraph.graph import StateGraph, START, END
+
+
+def chatbot(verbose: bool = False, track_time: bool = False, queue: Queue=None, env: Env=None) -> CompiledStateGraph:
+    chatbot_node = ChatbotNode(verbose=verbose, track_time=track_time, queue=queue, env=env)
+    graph = StateGraph(state_type=ChatbotState)
+    graph.add_node("chatbot", chatbot_node)
+    graph.add_edge(START, "chatbot")
+    graph.add_edge("chatbot", END)
+    memory_saver = MemorySaver()
+    return graph.compile(checkpoint=memory_saver)
 
 
