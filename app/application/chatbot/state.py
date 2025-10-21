e87d992b0f4e6a872e35a72bcf3e1a28b5494510
geonhee <pollux0718@gmail.com>
2025-10-22 04:07:17 +0900

    feat(state): add cost field with operator.add in ChatbotState

--- app/application/chatbot/state.py
+++ app/application/chatbot/state.py
@@ -1,7 +1,9 @@
 from typing import TypedDict, Annotated, Sequence
 from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage
+import operator
 
 class ChatbotState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
+    cost: Annotated[float, operator.add]
 
