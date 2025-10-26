abcad4eabcde52047a3906d3c54319c92fc67474
geonhee <pollux0718@gmail.com>
2025-10-27 02:50:48 +0900

    refactor(tool): change get_spec_description parameter key to runtime to apply langchain update

--- app/services/chatbot/tools/get_description.py
+++ app/services/chatbot/tools/get_description.py
@@ -1,21 +1,22 @@
+from dataclasses import dataclass
 from langchain.tools import tool, ToolRuntime
 from app.services.chatbot.tools.spec_description import ANALYSIS_SPEC_HELP
-from dataclasses import dataclass
-
 
 @dataclass
-class AnalysisSpecDescription:
+class Context:
     key: str
 
+
 @tool("get_spec_description", description="AnalysisSpec의 특정 키에 대한 설명을 가져옵니다.")
-def get_spec_description(runtime: ToolRuntime[AnalysisSpecDescription]):
+def get_spec_description(runtime: ToolRuntime[Context]):
     """
     AnalysisSpec의 특정 키에 대한 설명을 가져옵니다.
     Args:
-        runtime: ToolRuntime[AnalysisSpecDescription]
+        key: AnalysisSpec의 키
     Returns:
-        ANALYSIS_SPEC_HELP[key] (json)
+        AnalysisSpec의 키에 대한 설명 (json)
     """
+
     key = runtime.context.key
 
     spec = ANALYSIS_SPEC_HELP.get(key)
